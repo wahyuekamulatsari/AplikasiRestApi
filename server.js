3b9517f1eac5482250eb2e9578584ddcf353fd44
wahyuekamulatsari <wahyuekamalik@gmail.com>
2020-05-05 21:39:30 +0700

    menambahkan server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const bodyParser = require ('body-parser');
+const app = express();
+
+
+//parse app
+app.use(bodyParser.urlencoded( {extended: true}));
+app.use(bodyParser.json());
+
+
+app.listen(3000, () => {
+    console.log ('Server started on port');
+});
