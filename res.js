145131efbc61eb7ae7c07ecb2ebe726fd10685f2
wahyuekamulatsari <wahyuekamalik@gmail.com>
2020-05-16 16:44:50 +0700

    berhasil menghitung total harga service

--- res.js
+++ res.js
@@ -8,3 +8,38 @@ exports.ok = function (values, res) {
     res.json(data);
     res.end();
 }
+
+//response untuk nested servis
+exports.oknested = function(values, res){
+    //melakukan akumulasi
+    const hasil = values.reduce((akumulasikan, item)=>{
+        //metentukan key group
+        if(akumulasikan[item.nama_user]){
+            //membuat variabel group nama user
+            const group = akumulasikan[item.nama_user];            
+            //mengecek jika isi array adalah sparepart
+            if(Array.isArray(group.nama_sparepart, group.harga_sparepart, group.jumlah_sparepart, group.total)){
+                //tambahkan value ke dalam group sparepart
+                group.nama_sparepart.push(item.nama_sparepart);
+                group.harga_sparepart.push(item.harga_sparepart);
+                group.jumlah_sparepart.push(item.jumlah_sparepart);                
+            }else {
+                group.nama_sparepart = [group.nama_sparepart, item.nama_sparepart];
+                group.harga_sparepart = [group.harga_sparepart, item.harga_sparepart];
+                group.jumlah_sparepart = [group.jumlah_sparepart, item.jumlah_sparepart];                
+            }
+        }else {
+            akumulasikan[item.nama_user] = item;            
+        }
+        return akumulasikan;
+    }, {});
+
+    var data = {
+        'status':200,
+        'values':hasil
+    };
+    
+     res.json(data);
+     res.end();
+
+}
