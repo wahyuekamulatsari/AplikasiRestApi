edf0b5d5f7a7dc84a20ca45eaa8facdc36799bc3
wahyuekamulatsari <wahyuekamalik@gmail.com>
2020-05-17 11:35:33 +0700

    upload database

---
+++ db_bengkel.sql
@@ -0,0 +1,118 @@
+/*
+SQLyog Ultimate v11.11 (32 bit)
+MySQL - 5.1.37 : Database - db_bengkel
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_bengkel` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `db_bengkel`;
+
+/*Table structure for table `akses_token` */
+
+DROP TABLE IF EXISTS `akses_token`;
+
+CREATE TABLE `akses_token` (
+  `id_akses_token` int(20) NOT NULL AUTO_INCREMENT,
+  `id_user` int(10) DEFAULT NULL,
+  `acces_token` tinytext,
+  `ip_address` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id_akses_token`),
+  KEY `id_user` (`id_user`),
+  CONSTRAINT `akses_token_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `t_user` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `akses_token` */
+
+/*Table structure for table `t_level` */
+
+DROP TABLE IF EXISTS `t_level`;
+
+CREATE TABLE `t_level` (
+  `id_level` int(10) NOT NULL AUTO_INCREMENT,
+  `nama_level` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id_level`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `t_level` */
+
+insert  into `t_level`(`id_level`,`nama_level`) values (1,NULL);
+
+/*Table structure for table `t_montir` */
+
+DROP TABLE IF EXISTS `t_montir`;
+
+CREATE TABLE `t_montir` (
+  `id_montir` int(10) NOT NULL AUTO_INCREMENT,
+  `Nama_montir` varchar(20) DEFAULT NULL,
+  `harga_perjam` int(20) DEFAULT NULL,
+  PRIMARY KEY (`id_montir`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `t_montir` */
+
+insert  into `t_montir`(`id_montir`,`Nama_montir`,`harga_perjam`) values (1,'dedi',20000),(2,'fiki',25000);
+
+/*Table structure for table `t_service` */
+
+DROP TABLE IF EXISTS `t_service`;
+
+CREATE TABLE `t_service` (
+  `id_service` int(10) NOT NULL AUTO_INCREMENT,
+  `tgl_service` date DEFAULT NULL,
+  `id_user` int(10) DEFAULT NULL,
+  `id_montir` int(10) DEFAULT NULL,
+  `jumlah_sparepart` int(20) DEFAULT NULL,
+  `id_sparepart` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id_service`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `t_service` */
+
+insert  into `t_service`(`id_service`,`tgl_service`,`id_user`,`id_montir`,`jumlah_sparepart`,`id_sparepart`) values (1,NULL,NULL,NULL,NULL,NULL);
+
+/*Table structure for table `t_sparepart` */
+
+DROP TABLE IF EXISTS `t_sparepart`;
+
+CREATE TABLE `t_sparepart` (
+  `id_sparepart` int(10) NOT NULL AUTO_INCREMENT,
+  `nama_sparepart` varchar(20) DEFAULT NULL,
+  `harga_sparepart` int(20) DEFAULT NULL,
+  `satuan` int(20) DEFAULT NULL,
+  PRIMARY KEY (`id_sparepart`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `t_sparepart` */
+
+insert  into `t_sparepart`(`id_sparepart`,`nama_sparepart`,`harga_sparepart`,`satuan`) values (1,'Oli',20000,0),(2,'Ban',30000,0);
+
+/*Table structure for table `t_user` */
+
+DROP TABLE IF EXISTS `t_user`;
+
+CREATE TABLE `t_user` (
+  `id_user` int(10) NOT NULL AUTO_INCREMENT,
+  `nama_user` varchar(20) DEFAULT NULL,
+  `email` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `level` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `t_user` */
+
+insert  into `t_user`(`id_user`,`nama_user`,`email`,`password`,`level`) values (1,'Mila','mila@gmail.com','1234','1'),(2,'Vera','vera@gmail.com','4321','2');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
--- middleware/auth.js
+++ middleware/auth.js
@@ -99,3 +99,361 @@ exports.login = function (req, res) {
         }
     });
 }
+
+//Menambah data montir
+exports.tambahmontir = function (req, res) {
+    var post = {
+        nama_montir: req.body.nama_montir,
+        harga_perjam: req.body.harga_perjam
+    }
+
+    var query = "SELECT nama_montir FROM ?? WHERE ??=?";
+    var table = ["t_montir", "nama_montir", post.nama_montir];
+
+    query = mysql.format(query, table);
+
+    connection.query(query, function (error, rows) {
+        if (error) {
+            console.log(error);
+        } else {
+            if (rows.length == 0) {
+                var query = "INSERT INTO ?? SET ?";
+                var table = ["t_montir"];
+                query = mysql.format(query, table);
+                connection.query(query, post, function (error, rows) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        response.ok("Berhasil menambahkan data Montir baru", res);
+                    }
+                });
+            } else {
+                response.ok("Montir sudah terdaftar!", res);
+            }
+        }
+    });
+};
+
+//Menambah data sparepart
+exports.tambahsparepart = function (req, res) {
+    var post = {
+        nama_sparepart: req.body.nama_sparepart,
+        harga_sparepart: req.body.harga_sparepart,
+        satuan: req.body.satuan
+    }
+    var query = "SELECT nama_sparepart FROM ?? WHERE ??=?";
+    var table = ["t_sparepart", "nama_sparepart", post.nama_sparepart];
+    query = mysql.format(query, table);
+
+    connection.query(query, function (error, rows) {
+        if (error) {
+            console.log(error);
+        } else {
+            if (rows.length == 0) {
+                var query = "INSERT INTO ?? SET ?";
+                var table = ["t_sparepart"];
+                query = mysql.format(query, table);
+                connection.query(query, post, function (error, rows) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        response.ok("Berhasil menambahkan data Sparepart baru", res);
+                    }
+                });
+            } else {
+                response.ok("Sparepart sudah terdaftar!", res);
+            }
+        }
+    });
+};
+
+//Menambahkan data user
+exports.tambahuser = function (req, res) {
+    var post = {
+        nama_user: req.body.nama_user,
+        email: req.body.email,
+        password: md5(req.body.password),
+        role: req.body.role,
+
+    }
+    var query = "SELECT nama_user FROM ?? WHERE ??=?";
+    var table = ["t_user", "nama_user", post.nama_user];
+    query = mysql.format(query, table);
+    connection.query(query, function (error, rows) {
+        if (error) {
+            console.log(error);
+        } else {
+            if (rows.length == 0) {
+                var query = "INSERT INTO ?? SET ?";
+                var table = ["t_user"];
+                query = mysql.format(query, table);
+                connection.query(query, post, function (error, rows) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        response.ok("Berhasil menambahkan data user baru", res);
+                    }
+                });
+            } else {
+                response.ok("user sudah terdaftar!", res);
+            }
+        }
+    });
+};
+
+//Menambahkan data level
+exports.tambahlevel = function (req, res) {
+    var post = {
+        nama_level: req.body.nama_level
+    }
+
+    var query = "SELECT nama_level FROM ?? WHERE ??=?";
+    var table = ["t_level", "nama_level", post.nama_level];
+
+    query = mysql.format(query, table);
+
+    connection.query(query, function (error, rows) {
+        if (error) {
+            console.log(error);
+        } else {
+            if (rows.length == 0) {
+                var query = "INSERT INTO ?? SET ?";
+                var table = ["t_level"];
+                query = mysql.format(query, table);
+                connection.query(query, post, function (error, rows) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        response.ok("Berhasil menambahkan data level baru", res);
+                    }
+                });
+            } else {
+                response.ok("Level sudah terdaftar!", res);
+            }
+        }
+    });
+};
+
+//menambahkan data service
+exports.tambahservis = function (req, res) {
+    var post = {
+        tgl_service: new Date(),
+        id_user: req.body.id_user,
+        id_montir: req.body.id_montir,
+        jumlah_sparepart: req.body.jumlah_sparepart,
+        id_sparepart: req.body.id_sparepart
+    }
+    var query = "SELECT tgl_service FROM ?? WHERE ??=?";
+    var table = ["t_service", "tgl_service", post.tgl_service];
+    query = mysql.format(query, table);
+    connection.query(query, function (error, rows) {
+        if (error) {
+            console.log(error);
+        } else {
+            if (rows.length == 0) {
+                var query = "INSERT INTO ?? SET ?";
+                var table = ["t_service"];
+                query = mysql.format(query, table);
+                connection.query(query, post, function (error, rows) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        response.ok("Berhasil menambahkan data Servis baru", res);
+                    }
+                });
+            } else {
+                response.ok("Servis sudah terdaftar!", res);
+            }
+        }
+    });
+};
+
+//MENGUBAH DATA DI TABEL MONTIR
+exports.ubahmontir = function (req, res) {
+    var id_montir = req.body.id_montir;
+    var nama_montir = req.body.nama_montir;
+    var harga_perjam = req.body.harga_perjam;
+
+    connection.query('UPDATE t_montir SET nama_montir=?, harga_perjam=? WHERE id_montir=?',
+        [id_montir, nama_montir, harga_perjam,],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil Ubah Data Montir", res)
+            }
+        });
+};
+
+//mengubah data di tabel Sparepart
+exports.ubahsparepart = function (req, res) {
+    var id_sparepart = req.body.id_sparepart;
+    var nama_sparepart = req.body.nama_sparepart;
+    var harga_sparepart = req.body.harga_sparepart;
+    var satuan = req.body.satuan;
+
+    connection.query('UPDATE t_sparepart SET nama_sparepart=?, harga_sparepart=?, satuan=? WHERE id_sparepart=?',
+        [nama_sparepart, harga_sparepart, satuan, id_sparepart],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil Mengubah Data Sparepart", res)
+            }
+        });
+};
+
+//MENGUBAH DATA DI TABEL USER
+exports.ubahuser = function (req, res) {
+    var id_user = req.body.id_user;
+    var nama_user = req.body.nama_user;
+    var email = req.body.email;
+    var password = req.body.password;
+    var role = req.body.role;
+
+    connection.query('UPDATE t_user SET nama_user=?,email=?,password=?,role=? WHERE id_user=?',
+        [nama_user, email, password, role, id_user],
+
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil Ubah Data User", res);
+            }
+        });
+};
+//mengubah data di tabel level
+exports.ubahlevel = function (req, res) {
+    var id_level = req.body.id_level;
+    var nama_level = req.body.nama_level;
+
+    connection.query('UPDATE t_level SET nama_level=? WHERE id_level=?',
+        [id_level, nama_level],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil Mengubah Data Level", res)
+            }
+        });
+};
+
+//mengubah data di tabel service
+exports.ubahservice = function (req, res) {
+    var tgl_service = new Date();
+    var id_user = req.body.id_user;
+    var id_montir = req.body.id_montir;
+    var jumlah_sparepart = req.body.jumlah_sparepart;
+    var id_sparepart = req.body.id_sparepart;
+    var id_service = req.body.id_service;
+
+    connection.query('UPDATE t_servis SET tgl_service=?, id_user=?, id_montir=?, jumlah_sparepart=?, id_sparepart=? WHERE id_service=?',
+        [tgl_service, id_user, id_montir, jumlah_sparepart, id_sparepart, id_service],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil Mengubah Data servis", res)
+            }
+        });
+};
+
+//menghapus data di tabel montir
+exports.hapusmontir = function (req, res) {
+    var id_montir = req.body.id_montir;    
+
+    connection.query('DELETE FROM t_montir WHERE id_montir=?',
+    [id_montir],
+
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Data Montir Berhasil Dihapus", res);
+            }
+        });
+};
+
+//menghapus data di tabel sparepart
+exports.hapussparepart = function (req, res) {
+    var id_sparepart = req.body.id_sparepart;    
+
+    connection.query('DELETE FROM t_sparepart WHERE id_sparepart=?',
+    [id_sparepart],
+
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Data Sparepart Berhasil Dihapus", res);
+            }
+        });
+};
+
+//menghapus data di tabel user
+exports.hapususer = function (req, res) {
+    var id_user = req.body.id_user;    
+
+    connection.query('DELETE FROM t_user WHERE id_user=?',
+    [id_user],
+
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Data User Berhasil Dihapus", res);
+            }
+        });
+};
+
+//menghapus data di tabel level
+exports.hapuslevel = function (req, res) {
+    var id_level = req.body.id_level;    
+
+    connection.query('DELETE FROM t_level WHERE id_level=?',
+    [id_level],
+
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Data Level Berhasil Dihapus", res);
+            }
+        });
+};
+
+//menghapus data di tabel servis
+exports.hapusservice = function (req, res) {
+    var id_service = req.body.id_servis;    
+
+    connection.query('DELETE FROM t_servis WHERE id_service=?',
+    [id_service],
+
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Data Servis Berhasil Dihapus", res);
+            }
+        });
+};
+
+exports.totalharga = function (req, res) {
+    connection.query('SELECT t_user.nama_user, t_service.tgl_service, t_montir.nama_montir, t_sparepart.nama_sparepart, t_sparepart.harga_sparepart, t_service.jumlah_sparepart, (harga_perjam + jumlah_sparepart * harga_sparepart) AS total_harga FROM t_service JOIN t_user JOIN t_montir JOIN t_sparepart WHERE t_service.id_user = t_user.id_user AND t_service.id_montir = t_montir.id_montir AND t_service.id_sparepart = t_sparepart.id_sparepart ORDER BY t_user.id_user ',
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.oknested(rows, res);
+            }
+        }
+    )
+
+}
+
+exports.halamanpelanggan = function (req, res) {
+    response.ok("Halaman ini hanya untuk admin dengan level = 2!", res);
+}
+exports.halamanadmin = function (req, res) {
+    response.ok("Halaman ini hanya untuk pelanggan dengan level = 1!", res);
+}
